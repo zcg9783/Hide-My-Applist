b99cab19318fe7d710b51c57fc51af0d5a7c47ea
zcg9783 <zcg5665@126.com>
2025-07-20 15:16:53 +0800

    更新 Constants.java

--- common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
@@ -4,9 +4,9 @@ import java.util.Set;
 
 public class Constants {
 
-    public static final String APP_PACKAGE_NAME = "com.tsng.hidemyapplist";
+    public static final String APP_PACKAGE_NAME = "com.bilibili.app.in";
 
-    public static final String PROVIDER_AUTHORITY = APP_PACKAGE_NAME + ".ServiceProvider";
+    public static final String PROVIDER_AUTHORITY = "com.tsng.hidemyapplist.ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String UPDATE_URL_BASE = "https://api.nullptr.icu/android/hide-my-applist/static/";
